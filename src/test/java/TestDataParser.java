c125ed020883170112e5c54033a24ac5b0e36153
Roman Turchin <roman@turch.in>
2021-02-23 20:11:49 +0300

    generify data reader

---
+++ src/test/java/TestDataParser.java
@@ -0,0 +1,20 @@
+import com.hcl.footballkata.Pair;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestDataParser {
+   /*@Test
+    public void testFileParsing () {
+        assertThat(FootballSource.stream("test-data.dat").count())
+            .isEqualTo(2)
+            .as("description");
+    }
+
+    @Test
+    public void testLineParser() {
+        Pair<String, Integer> expected = Pair.of("Tottenham", 4);
+        assertThat(produceNameAndResult("    9. Tottenham       38    14   8  16    49  -  53    50"))
+            .isEqualTo(expected);
+    }*/
+}
